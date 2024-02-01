96bd5d210937c99528bbd5a2fc2a88ba5ed81ca7
siggnut <157001672+siggnut@users.noreply.github.com>
2024-02-01 14:39:36 +0200

    Add files via upload

---
+++ C07/vs_code_rush00.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+void    rush(int x, int y);
+
+int    main(void)
+{
+    rush(7, 8);
+    return (0);
+}
+
+void    ft_putchar(char c)
+{
+    write(1, &c, 1);
+}
+
+void	first_or_last(int square_number, int width)
+{
+	while (square_number < width)
+	{
+		if (square_number == 0 || square_number == width - 1)
+		{
+			ft_putchar('o');
+		}
+		else
+		{
+			ft_putchar('-');
+		}
+		square_number++;
+	}
+}
+
+void	middle(int square_number, int width)
+{
+	while (square_number < width)
+	{
+		if (square_number == 0 || square_number == width - 1)
+		{
+			ft_putchar('|');
+		}
+		else
+		{
+			ft_putchar(' ');
+		}
+		square_number++;
+	}
+}
+
+void	print_line(int row_number, int width, int height)
+{
+	int	square_number;
+
+	square_number = 0;
+	if (row_number == 0 || row_number == height - 1)
+	{
+		first_or_last(square_number, width);
+	}
+	else
+	{
+		middle(square_number, width);
+	}
+}
+
+void	rush(int x, int y)
+{
+	int	row_number;
+
+	row_number = 0;
+	while (row_number < y)
+	{
+		print_line(row_number, x, y);
+		if (row_number < y - 1)
+		{
+			ft_putchar('\n');
+		}
+		row_number++;
+	}
+}
